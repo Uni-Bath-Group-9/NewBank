448264a28d95d6f4cbbe41e730fd2124fc14e2ec
Gabriel Oduori <gabriel.oduori@gmail.com>
2020-11-25 14:55:47 +0300

    Customer class
    Completed the constructor method for the Customer class by adding two variables of customerID and password.
    Created an instance of a customer which takes an array of accounts, customerID and a password.
    Added two methods isPasswordCorrect, which checks is a user has entered the correct password and getCustomerID which returns current customers' ID.

--- server/Customer.java
+++ server/Customer.java
@@ -5,15 +5,19 @@ import java.util.ArrayList;
 public class Customer {
 	
 	private ArrayList<Account> accounts;
+	private String customerID;
+	private String password;
 	
-	public Customer() {
+	public Customer(String customerID, String password) {
 		accounts = new ArrayList<>();
+		this.customerID = customerID.toLowerCase();
+		this.password = password;
 	}
 	
 	public String accountsToString() {
 		String s = "";
 		for(Account a : accounts) {
-			s += a.toString();
+			s += a.toString()+"\n";
 		}
 		return s;
 	}
@@ -21,4 +25,14 @@ public class Customer {
 	public void addAccount(Account account) {
 		accounts.add(account);		
 	}
+
+	public boolean isPasswordCorrect(String password){
+		return this.password.equals(password);
+	}
+
+	public String getCustomerID(){
+		return this.customerID;
+	}
+
+
 }
